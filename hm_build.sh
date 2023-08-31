a0f74f2879ad1c9062028d9e8cd394e71854605b
tang584 <meng.tang@pnnl.gov>
2023-08-30 21:05:08 -0700

    build options

--- hm_build.sh
+++ hm_build.sh
@@ -1,14 +1,47 @@
   
-HERMES_PATH=$HOME/install/vfd_hermes
-HERMES_REPO=/qfs/people/tang584/hermes_stage/dec_hermes-vfd
+HERMES_REPO=/people/tang584/hermes_stage/hermes-vfd
 
 # source $HERMES_REPO/load_hermes_deps.sh
 
 mkdir -p $HERMES_REPO/build
+
 cd $HERMES_REPO/build
 
-build_type="Release" # Release, RelWithDebInfo, Debug
+# build_type="Release" # Release, RelWithDebInfo, Debug
 
+build_type="$1"
+clean_build="$2"
+
+# 0: Release, 1: RelWithDebInfo, 2: Debug
+if [ "$build_type" == "0" ]; then
+  build_type="Release"
+  HERMES_PATH=$HOME/install/vfd_hermes
+
+elif [ "$build_type" == "1" ]; then
+  build_type="RelWithDebInfo"
+  HERMES_PATH=$HOME/install/vfd_debug_hermes
+
+elif [ "$build_type" == "2" ]; then
+  build_type="Debug"
+  HERMES_PATH=$HOME/install/vfd_debug_hermes
+
+else
+  echo "Invalid build type: $build_type"
+  # display instructions
+  echo "Usage: ./hm_build.sh [build_type] [optional: clean_build]"
+  echo "build_type: 0: Release, 1: RelWithDebInfo, 2: Debug"
+  echo "clean_build: 0: clean, 1: not clean"
+  exit 1
+fi
+
+# If there is option 2, clean the build
+if [ "$clean_build" == "0" ]; then
+  echo "Clean build"
+  rm -rf $HERMES_REPO/build/*
+  rm -rf $HERMES_PATH
+fi
+
+echo "Build type: $build_type"
 
 cmake ../ -DCMAKE_BUILD_TYPE=$build_type \
   -DCMAKE_INSTALL_PREFIX=$HERMES_PATH \
